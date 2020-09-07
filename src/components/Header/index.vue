96a74d2f9ec69dfe3a7f1ab8511df58c140e4af5
AlbertBoys <1430292539@qq.com>
2020-09-07 12:36:18 +0800

    2222

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -53,7 +53,7 @@ export default {
   name: "Header",
   methods: {
     toSearch() {
-      this.$router.push('/search').then(()=>{})
+      this.$router.push('/search').then(()=>{})//111
     }
   }
 };
